9211c96d7425565899ec3d7b7fd546c466a61d3d
ilirg <ilirg@cacttus.com>
2020-05-31 16:01:14 +0200

    Initial Commit

---
+++ DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetValues()
+        {
+            return Ok(await _context.Values.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetValue(int id)
+        {
+            Value value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            if (value == null)
+                return NotFound();
+
+            return Ok(value);
+        }
+    }
+}
